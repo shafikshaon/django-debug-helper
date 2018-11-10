fab20d24e9ef2e51bff643f8e4327990d5872f80
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-11-10 12:35:16 +0600

    version upgraded

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-debug-helper",
-    version="1.1.4",
+    version="1.1.6",
     author="Mohd. Shafikur Rahman",
     author_email="shafikshaon@gmail.com",
     description="Django debug error related exception search on Google/ Stack Overflow.",
